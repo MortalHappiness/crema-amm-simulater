c6a5dc2cac2dbf44c1ae7f577288110ba445dbd6
jesChung <gracefuleverymoment@gmail.com>
2022-04-23 19:17:22 +0800

    tailwind

--- core/components/Header.jsx
+++ core/components/Header.jsx
@@ -1,6 +1,9 @@
 const Header = () => {
     return (
-        <h1 className="text-red-500">Crema Amm Simulater</h1>
+        <div className="max-h-12 sm:max-h-16">
+            <h1 className="text-grey-500 text-xl px-6 ">Crema Amm Simulater</h1>
+            <hr />
+        </div>
     );
 }
  
