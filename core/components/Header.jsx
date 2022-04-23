eec772d7b4d714130a10f16013ae8a288b112c34
jillchang <jill.chang@jkos.com>
2022-04-23 19:32:40 +0800

    fix: header

--- core/components/Header.jsx
+++ core/components/Header.jsx
@@ -1,17 +1,6 @@
 import PageHeader from "antd/lib/page-header";
 
 const Header = () => {
-<<<<<<< HEAD
-    return (
-        <div className="max-h-12 sm:max-h-16">
-            <h1 className="text-grey-500 text-xl px-6 ">Crema Amm Simulater</h1>
-            <hr />
-        </div>
-    );
-}
- 
-export default Header;
-=======
   return (
     <PageHeader
       className="site-page-header bg-white"
@@ -22,4 +11,3 @@ export default Header;
 };
 
 export default Header;
->>>>>>> c6bd672fe5c7159d177847e0882df0a18d058edc
